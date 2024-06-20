462a874642dcfe9c532dcb15e72a62fc4e08a927
Maliya11 <114799896+Maliya11@users.noreply.github.com>
2024-06-20 16:39:58 +0530

    read_sensor

---
+++ Firmware/TestBluePill/include/read_sensor.h
@@ -0,0 +1,11 @@
+
+
+#ifndef INCLUDE_READ_SENSOR_H_
+#define INCLUDE_READ_SENSOR_H_
+
+#include <Arduino.h>
+void test(void);
+
+
+
+#endif
--- Firmware/TestBluePill/src/main.cpp
+++ Firmware/TestBluePill/src/main.cpp
@@ -1,18 +1,14 @@
 #include <Arduino.h>
+#include "read_sensor.h"
 
-#define LED_PIN PC13
 
-void setup() {
-  // Initialize the digital pin as an output.
-  pinMode(LED_PIN, OUTPUT);
+
+void setup()
+{
+  test();
 }
 
-void loop() {
-  // Turn the LED on
-  digitalWrite(LED_PIN, LOW);   // Note: The LED is active low on the BluePill
-  delay(500);                  // Wait for half a second
+void loop()
+{
   
-  // Turn the LED off
-  digitalWrite(LED_PIN, HIGH);
-  delay(500);                  // Wait for half a second
 }
