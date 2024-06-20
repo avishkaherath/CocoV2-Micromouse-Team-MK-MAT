215974de8e193c4b8fce93f854358f656d6a440b
Avishka Herath <achamuditha@gmail.com>
2024-06-20 22:18:36 +0530

    Tested UART2

--- Firmware/TestBluePill/src/main.cpp
+++ Firmware/TestBluePill/src/main.cpp
@@ -1,23 +1,38 @@
 #include <Arduino.h>
-#include "ICMSensor.h"
+// #include "ICMSensor.h"
 
-ICMSensor sensor;
+// ICMSensor sensor;
 
-#define LED_PIN PC13
+// #define LED_PIN PC13
+
+// void setup() {
+//   sensor.setup();
+//   // Initialize the digital pin as an output.
+//   pinMode(LED_PIN, OUTPUT);
+// }
+
+// void loop() {
+//     sensor.loop();
+//   // Turn the LED on
+//   digitalWrite(LED_PIN, LOW);   // Note: The LED is active low on the BluePill
+//   delay(500);                  // Wait for half a second
+  
+//   // Turn the LED off
+//   digitalWrite(LED_PIN, HIGH);
+//   delay(500);                  // Wait for half a second
+// }
+
+#include <HardwareSerial.h>
+
+HardwareSerial Serial2(PA3, PA2); // RX, TX
 
 void setup() {
-  sensor.setup();
-  // Initialize the digital pin as an output.
-  pinMode(LED_PIN, OUTPUT);
+  // Initialize Serial2 at 115200 bps
+  Serial2.begin(115200);
 }
 
 void loop() {
-    sensor.loop();
-  // Turn the LED on
-  digitalWrite(LED_PIN, LOW);   // Note: The LED is active low on the BluePill
-  delay(500);                  // Wait for half a second
-  
-  // Turn the LED off
-  digitalWrite(LED_PIN, HIGH);
-  delay(500);                  // Wait for half a second
+  // Print a message every second
+  Serial2.println("Hello from UART2!");
+  delay(1000); // Delay for 1000 milliseconds (1 second)
 }
