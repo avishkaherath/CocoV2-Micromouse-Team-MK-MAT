70c4e8940939619abb8e2c38f2e4be00c8312685
Maliya11 <114799896+Maliya11@users.noreply.github.com>
2024-06-21 10:26:45 +0530

    read_sensor and adc

---
+++ Firmware/TestingCode/include/typedefs.h
@@ -0,0 +1,11 @@
+#include <stdint.h>
+
+
+typedef enum adc_channels
+{
+	LF_RECEIVER = 0,
+	RF_RECEIVER,
+	DR_RECEIVER,
+	DL_RECEIVER,
+	BAT_VOL
+} adc_channels;
---
+++ Firmware/TestingCode/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include "read_sensor.h"
+
+void setup() {
+  readSensor();
+}
+
+void loop() {
+  
+}
+
